95859816ac96ea42e010bc05f35aa6baf582616d
lofi4ever <gai@one-touch.ru>
2020-05-14 02:42:30 +0300

    authentication works somehow

--- api/index.js
+++ api/index.js
@@ -2,12 +2,25 @@ const http = require('http');
 const express = require('express');
 const mongoose = require('mongoose');
 
+//middlewares
+const jwt = require('./middlewares/jwt');
+const errorHandler = require('./helpers/error-handler');
+
 const tasksRouter = require('./tasks/router');
+const userRouter = require('./user/user.router');
 
 const handler = express();
 
 handler
-  .use('/api/tasks', tasksRouter);
+  .use(jwt())
+  .use(function(req, res, next) {
+    console.log('logger');
+    next();
+  })
+  .use('/api/tasks', tasksRouter)
+  .use('/api/user', userRouter);
+
+handler.use(errorHandler);
 
 mongoose.connect('mongodb://localhost:27017/tasks-manager-db', {useNewUrlParser: true}, (err) => {
   if(err) return console.log(err);
